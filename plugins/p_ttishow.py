f95db385922dfd494e7a698fa416613d6dbfab29
KOTDEOMOREPOS <90854597+KOTDEOMOREPOS@users.noreply.github.com>
2021-11-17 21:18:43 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('Help 🆘', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/KristyBots')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/KOT_BOTS')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
